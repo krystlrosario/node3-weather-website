58d2f228e55c5392955f3a9632e026afa6f63987
Krystl Rosario <krystlrosario.bsee@gmail.com>
2021-08-02 10:14:39 +0800

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("Client side javascript file is loaded!");
+// console.log("Client side javascript file is loaded!");
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
@@ -7,7 +7,7 @@ const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
+ 
     const location = search.value;
     
     messageOne.textContent = 'Loading ...';
