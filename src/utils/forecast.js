fb94e785127fe609fccd410353f92ca4792fc3be
Krystl Rosario <krystlrosario.bsee@gmail.com>
2021-08-02 13:33:55 +0800

    Add humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,6 +17,26 @@
 // module.exports = forecast;
 
 ////////////// DESTRUCTURING //////////////
+// const request = require('request');
+
+// const forecast = (longitude, latitude, callback) => {
+//     const url = `http://api.weatherstack.com/current?access_key=0486f6999200ad88dbfc7900f5f95af5&query=${encodeURIComponent(longitude, latitude)}`;
+
+//     request({url, json: true}, (error, {body}) => {
+//         if(error) {
+//             callback("Unable to connect with weather services", undefined);
+//         } else if (body.error) {
+//             callback("No matching results of the give coordinates.", undefined)
+//         } else {
+//             callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+//         }
+//     })
+// };
+
+// module.exports = forecast;
+
+////////////// NEW FEATURE DEPLOYMENT WORKFLOW //////////////
+
 const request = require('request');
 
 const forecast = (longitude, latitude, callback) => {
@@ -28,7 +48,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback("No matching results of the give coordinates.", undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`)
         }
     })
 };
